7cbbcb5f0fbfb5b1d4998023d58d33563a745c10
sygessese <51281147+sygessese@users.noreply.github.com>
2020-11-18 04:04:32 -0800

    Revert "Sourcecode"

--- src/components/CvSection.js
+++ src/components/CvSection.js
@@ -1,5 +1,36 @@
 import PropTypes from "prop-types"
 import React from "react"
+<<<<<<< HEAD
+import styled from 'styled-components'
+import CvSectionItem from './CvSectionItem'
+
+
+const CvSection = ({ section, contents }) => {
+
+  const Section = styled.div`
+  display: flex;
+  flex-direction: row;
+  @media (max-width: 820px) {
+    margin-left: 1em;
+    margin-right: 1em;
+    }
+  `
+  const SectionTitle = styled.div`
+  font-style: italic;
+  width: 10em;
+  @media (max-width: 820px) {
+    width: 8em;
+    }
+  `
+  const SectionContent = styled.div`
+  width: 100%;
+  `
+  return (
+    <Section>
+      <SectionTitle>
+        {section}
+      </SectionTitle>
+=======
 import styled from "styled-components"
 import CvSectionItem from "./CvSectionItem"
 
@@ -27,12 +58,17 @@ const CvSection = ({ section, contents }) => {
   return (
     <Section>
       <SectionTitle>{section}</SectionTitle>
+>>>>>>> master
       <SectionContent>
         {contents.map(content => {
           return <CvSectionItem item={content} />
         })}
       </SectionContent>
     </Section>
+<<<<<<< HEAD
+
+=======
+>>>>>>> master
   )
 }
 
@@ -42,8 +78,13 @@ CvSection.propTypes = {
 }
 
 CvSection.defaultProps = {
+<<<<<<< HEAD
+  section: 'section-title-mission',
+  contents: [{}]
+=======
   section: "section-title-mission",
   contents: [{}],
+>>>>>>> master
 }
 
 export default CvSection
